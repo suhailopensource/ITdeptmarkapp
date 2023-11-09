064c5693f5e6c352a9d58ec28323114354fb8de6
Suhail <suhailcollegemail@gmail.com>
2023-11-09 15:06:42 +0530

    camera and capture photo button ui  done

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
+
+  const cameraRef = useRef(null);
+
+  if (!permission) {
+
+    return <View />;
+  }
+
+  if (!permission.granted) {
+
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="Grant Permission" />
+      </View>
+    );
+  }
+
+  async function takePicture() {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      setCapturedPhoto(photo.uri);
+
+
+      // await MediaLibrary.saveToLibraryAsync(photo.uri);
+    }
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+
+      {capturedPhoto && (
+        <View style={styles.previewContainer}>
+          <Text style={styles.text}>Captured Photo:</Text>
+          <View style={styles.previewImageContainer}>
+            <Image source={{ uri: capturedPhoto }} style={styles.previewImage} />
+          </View>
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +67,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  previewContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  previewImageContainer: {
+    marginTop: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  previewImage: {
+    width: 200,
+    height: 300,
   },
 });
